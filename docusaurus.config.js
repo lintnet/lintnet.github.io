f3eecf6cf255817cf3e02c8778a9d1657ffe432e
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2024-01-15 21:36:13 +0900

    update catch copy (#48)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import { themes as prismThemes } from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'lintnet',
-  tagline: 'Configuration linter powered by Jsonnet',
+  tagline: 'Powerful, Secure, Shareable linter powered by Jsonnet',
   // favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
