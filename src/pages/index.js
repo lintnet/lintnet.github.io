48bd9de471981cd5a9affeaab39cb0efddc209de
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-02-02 06:57:09 +0900

    add logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
+        <img src="https://raw.githubusercontent.com/lintnet/logo/add-logo/images/lintnet.png" alt="lintnet Logo" className="top__logo" />
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
