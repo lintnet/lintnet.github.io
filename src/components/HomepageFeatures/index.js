f070dc60c3d619d41e8ce3b8fec42bae39b922a8
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-05-25 17:09:30 +0900

    update description

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,12 +4,24 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
+    title: 'General purpose linter',
+    description: (
+      <>
+        lintnet is a general purpose linter that can be used for any kind of files.
+        lintnet is a command line tool like <a href="https://www.conftest.dev/" target="_blank">Conftest</a>.
+        lintnet doesn't have any built-in lint rules, but you can define lint rules yourself freely.
+        lintnet supports <a href="/docs/supported-data-format/">various file formats such as JSON, YAML, TOML, HCL, and so on</a>.
+      </>
+    ),
+  },
+  {
     title: 'Powered by Jsonnet',
     description: (
       <>
-        <a href="https://jsonnet.org/" target="_blank">Jsonnet</a> is a simple and powerful configuration language for app and tool developers.
-        If you're already familiar with Jsonnet, you can easily start lintnet.
-        Even if you're not familiar with Jsonnet, no worries. Jsonnet is easy to understand and the learning cost is low.
+        lintnet uses <a href="https://jsonnet.org/" target="_blank">Jsonnet</a> for configuration files and lint rules.
+        Jsonnet is a well-known, simple, and powerful configuration language.
+        Jsonnet is a superset of JSON, and can generate JSON dinamically.
+        Jsonnet is easy to understand and the learning cost is low.
         Jsonnet is widely used, so you can utilize the knowledge and experience of Jsonnet in not only lintnet but also other projects.
       </>
     ),
@@ -18,7 +30,7 @@ const FeatureList = [
     title: 'Secure',
     description: (
       <>
-        Jsonnet can't access filesystem and network so it's secure compared with common programming languages such as JavaScript
+        Jsonnet restricts access to filesystem and network so it's secure compared with common programming languages such as Python
       </>
     ),
   },
@@ -26,9 +38,8 @@ const FeatureList = [
     title: 'Shareable',
     description: (
       <>
-        lintnet provides Module system that you can share and reuse lint rules between other projects.
-        You can develop lint rules as both OSS and internal libraries.
-        Please see <a href="/docs/module/">Module</a> too.
+        lintnet supports sharing lint rules as <a href="/docs/module/">Modules</a>.
+        You can utilize third party lint rules, reuse your lint rules in multiple projects, and distribute lint rules as OSS and in-house libraries
       </>
     ),
   },
